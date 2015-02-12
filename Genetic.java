f4c32ad41fb80c58a4ee2a20db81b93da6d292e1
Zackery Leman <zleman1593@gmail.com>
2015-02-11 21:01:17 -0500

    just need to add selection methods, UC, and debug

--- Genetic.java
+++ Genetic.java
@@ -2,43 +2,77 @@ import java.util.*;
 
 
 public class Genetic {
-	private ArrayList<ArrayList<Integer>> population = new ArrayList<ArrayList<Integer>>();
+	public ArrayList<ArrayList<Integer>> population = new ArrayList<ArrayList<Integer>>();
 	private ArrayList<ArrayList<Integer>> satProblem = new ArrayList<ArrayList<Integer>>();
-	private Random random; //= new Random;
+	private Random random; 
+	private int maxIteration;
+	private double crossOverProb; 
+	private double mutateProb; 
 
 
-	public Genetic(int popSize, int literalNumber,ArrayList<ArrayList<Integer>> satProblem) {
+	public Genetic(int popSize, int literalNumber, int maxIteration, double crossOverProb, double mutateProb,ArrayList<ArrayList<Integer>> satProblem) {
 		this.satProblem = satProblem;
 		population = initPopulation(popSize,literalNumber);
 		this.random = new Random();
+		this.maxIteration = maxIteration;
+		this.crossOverProb = crossOverProb;
+		this.mutateProb = mutateProb;
 	}
 
+	
+	public void evolve(ArrayList<ArrayList<Integer>> popToEvolve){
+		boolean foundSATSolution = false;
+		for (int i = 0; i < maxIteration && !foundSATSolution; i++){
+			
+			singlePointCrossover(crossOverProb, popToEvolve);
+			mutate(mutateProb, popToEvolve);
+			
+		
+			
+		}
+	}
+	
+	
 	private void mutate(double mutateProb,ArrayList<ArrayList<Integer>> popToMutate ) {
 		for (int i = 0; i < popToMutate.size() ;i++){
 			for (int j = 0; j < popToMutate.get(i).size() ;j++){
 				boolean flip = random.nextDouble() < mutateProb;
 				if (flip &&  popToMutate.get(i).get(j) == 1){
-					 popToMutate.get(i).set(j,0);
+					popToMutate.get(i).set(j,0);
 				}else if(flip && popToMutate.get(i).get(j) == 0){
-					 popToMutate.get(i).set(j,1);
+					popToMutate.get(i).set(j,1);
 				}
 			}
 
 		}
 	}
-	
+
 	private void singlePointCrossover(double crossProb,ArrayList<ArrayList<Integer>> popToCross ) {
 		for (int i = 0; i < popToCross.size() ;i+= 2){
 			boolean cross = random.nextDouble() < crossProb;
+			int crossOverLocation;
 			if (cross){
-			
-	
+				//Pick cross over location
+				crossOverLocation = random.nextInt(popToCross.get(i).size());
+				//Copy first half of A into C
+				List<Integer> c =  popToCross.get(i).subList(0, crossOverLocation); 
+				//Replace first half of A with First half of B
+				for (int j = 0; j < popToCross.get(i).size() ;j++){
+					int value = popToCross.get(i+1).get(j);
+					popToCross.get(i).set(j, value);
+				}
+				//Replace first half of B with C
+				for (int j = 0; j < c.size() ;j++){
+					int value = c.get(j);
+					popToCross.get(i+1).set(j, value);
+				}
+
 			}
 
 		}
 	}
 
-/*
+	/*
 	public ArrayList<ArrayList<Integer>> tournament(int winners, int sample) {
 		ArrayList<ArrayList<Integer>> winnerPool = new ArrayList<ArrayList<Integer>>();
 		while( winnerPool.size() < population.size()){
@@ -55,6 +89,7 @@ public class Genetic {
 
 		return winnerPool;
 	}*/
+	
 	public ArrayList<ArrayList<Integer>> initPopulation(int popSize,int literalNumber){
 		ArrayList<ArrayList<Integer>> population = new ArrayList<ArrayList<Integer>>();
 		for (int i = 0; i < popSize ;i++){
@@ -65,7 +100,6 @@ public class Genetic {
 				individual.add(value);
 			}
 			population.add(individual);
-
 		}
 		return population;
 	}
--- controller.java
+++ controller.java
@@ -6,18 +6,23 @@ public class controller {
 	
 	public static void main(String [ ] args) {
 		String infile = "/Users/zackleman/Desktop/brock200_1.clq.cnf";
-		int numberOfLiterals = 0;
+		int numberOfLiterals = 10;
 		int popSize = 10;
+		int maxIteration = 10;
+		double crossOverProb = 0.5; 
+	    double mutateProb = 0.5; 
 		
 		SatProblem problem = new SatProblem();
 		problem.createSatProblemFromFile(infile);
 		numberOfLiterals = problem.getNumLiterals();
 		
 		
-		Genetic geneticAlgo = new Genetic(popSize,numberOfLiterals,problem.getProblem());
-		//ArrayList<ArrayList<Integer>>  pop = geneticAlgo.population;
-		//geneticAlgo.mutate(0.5, pop);
-
+		Genetic geneticAlgo = new Genetic(popSize, numberOfLiterals, maxIteration, crossOverProb, mutateProb,problem.getProblem());
+		
+		ArrayList<ArrayList<Integer>>  pop = geneticAlgo.population;
+		geneticAlgo.evolve(pop);
+		//geneticAlgo.singlePointCrossover(1, pop);
+//int test = 0;
 		
 	}
 }
